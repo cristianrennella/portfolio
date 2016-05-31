f85d21299133805cc570afd5bcc7ea98f6e7c7e4
codecademy <ccuser@codecademy.com>
2016-05-31 17:32:06 -0400

    chau

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'pages#welcome'
   get 'welcome' => 'pages#welcome'
   get 'portfolio' => 'portfolio#portfolio'
+  get 'about' => 'about#about'
+  get 'contact' => 'contact#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
